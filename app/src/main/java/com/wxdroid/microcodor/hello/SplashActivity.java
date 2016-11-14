c08fc53363c99b307a889729409fb809a3c3b93a
jinchun <jinchun8023@163.com>
2016-11-15 01:39:16 +0800

    add toolbar menu

--- app/src/main/java/com/wxdroid/microcodor/hello/SplashActivity.java
+++ app/src/main/java/com/wxdroid/microcodor/hello/SplashActivity.java
@@ -2,10 +2,14 @@ package com.wxdroid.microcodor.hello;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.wxdroid.microcodor.ui.MainActivity;
 import com.wxdroid.microcodor.R;
 import com.wxdroid.microcodor.model.MsgEvent;
@@ -20,25 +24,24 @@ import java.util.TimerTask;
  */
 
 public class SplashActivity extends Activity {
-    ImageView logo;
+    private SimpleDraweeView dvWelcome;
 
     final String json = "Test EventBus";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);//放在加载布局之前
         setContentView(R.layout.activity_splash);
-       /* if (!EventBus.getDefault().isRegistered(this)){
-            EventBus.getDefault().register(this);
-        }*/
-        logo = (ImageView) findViewById(R.id.logo);
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                MsgEvent msgEvent = new MsgEvent(json);
-
-            }
-        });
+        dvWelcome= (SimpleDraweeView) findViewById(R.id.dv_welcome);
+        /**
+         * 下面是主要代码：
+         */
+        DraweeController draweeController = Fresco.newDraweeControllerBuilder()
+                .setAutoPlayAnimations(true)//自动播放动画
+                .setUri(Uri.parse("asset://com.wxdroid.microcodor/gif/gif45.gif"))//路径
+                .build();
+        dvWelcome.setController(draweeController);
 
 
         Timer timer = new Timer();
